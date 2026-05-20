f2810a1c14c9d02479a74ff87fbad5993ae64028
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2026-05-20 19:13:23 +0700

    disabling lsp, lint, formating

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,15 @@
+return {
+  { "neovim/nvim-lspconfig", enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+
+  { "stevearc/conform.nvim", enabled = false },
+  { "mfussenegger/nvim-lint", enabled = false },
+
+  { "windwp/nvim-ts-autotag", enabled = false },
+
+  { "folke/todo-comments.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+
+  { "lewis6991/gitsigns.nvim", enabled = false },
+}
